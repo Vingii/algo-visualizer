bcfe773819c79f57404e560c8ab5ff5b7a771e73
Vingii <50549345+Vingii@users.noreply.github.com>
2022-04-29 01:16:43 +0200

    unified algo htmls

--- js/algos.js
+++ js/algos.js
@@ -5,12 +5,12 @@ var links = ["search"];
 function makeCards() {
 	for (var i = 0; i < names.length; i++) {
 		document.getElementById('card-deck').innerHTML += '<div class="col mt-3"><div class="card text-white bg-primary h-100"><div class="card-body"><h4 class="card-title">'
-			+ names[i] + '</h4><p class="card-text">' + descs[i] + '</p><a href="' + links[i] + '.html" class="stretched-link"></a></div></div></div>';
+			+ names[i] + '</h4><p class="card-text">' + descs[i] + '</p><a href="/algo.html?algo=' + links[i] + '" class="stretched-link"></a></div></div></div>';
 	};
 };
 
 function fillNav() {
 	for (var i = 0; i < names.length; i++) {
-		document.getElementById('navbar-algo').innerHTML += '<a href="/' + links[i] + '.html" class="dropdown-item">' + names[i] + '</a>';
+		document.getElementById('navbar-algo').innerHTML += '<a href="/algo.html?algo=' + links[i] + '" class="dropdown-item">' + names[i] + '</a>';
 	};
 };
